90513dec0cba12bf6822b4b3f66303bcb7371cd9
Rajeshkumar952 <rajeshkumar9523rama@gmail.com>
2024-06-22 17:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 https://www.svgrepo.com/show/345221/three-dots.svg//your JS code here. If required.
-https://www.svgrepo.com/show/345221/three-dots.svg
+https://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svg
